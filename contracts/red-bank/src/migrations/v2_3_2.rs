36f639ff31cb895393a6a652e2585b5553e52186
mark <mark@marsprotocol.foundation>
2025-12-18 23:38:41 +1000

    testing and migration

---
+++ contracts/red-bank/src/migrations/v2_3_2.rs
@@ -0,0 +1,18 @@
+use cosmwasm_std::{DepsMut, Response};
+use cw2::{assert_contract_version, set_contract_version};
+
+use crate::{contract::CONTRACT_NAME, error::ContractError};
+
+pub const FROM_VERSION: &str = "2.3.1";
+pub const TO_VERSION: &str = "2.3.2";
+
+pub fn migrate(deps: DepsMut) -> Result<Response, ContractError> {
+    assert_contract_version(deps.storage, &format!("crates.io:{CONTRACT_NAME}"), FROM_VERSION)?;
+
+    set_contract_version(deps.storage, format!("crates.io:{CONTRACT_NAME}"), TO_VERSION)?;
+
+    Ok(Response::new()
+        .add_attribute("action", "migrate")
+        .add_attribute("from_version", FROM_VERSION)
+        .add_attribute("to_version", TO_VERSION))
+}
--- contracts/red-bank/tests/tests/test_migration_v2.rs
+++ contracts/red-bank/tests/tests/test_migration_v2.rs
@@ -111,3 +111,57 @@ fn v2_3_0_to_v2_3_1_successful_migration() {
     };
     assert_eq!(cw2::get_contract_version(deps.as_ref().storage).unwrap(), new_contract_version);
 }
+
+#[test]
+fn v2_3_1_to_v2_3_2_wrong_contract_name() {
+    let mut deps = mock_dependencies(&[]);
+    cw2::set_contract_version(deps.as_mut().storage, "contract_xyz", "2.3.1").unwrap();
+
+    let err = migrate(deps.as_mut(), mock_env(), MigrateMsg::V2_3_1ToV2_3_2 {}).unwrap_err();
+
+    assert_eq!(
+        err,
+        ContractError::Version(VersionError::WrongContract {
+            expected: "crates.io:mars-red-bank".to_string(),
+            found: "contract_xyz".to_string()
+        })
+    );
+}
+
+#[test]
+fn v2_3_1_to_v2_3_2_wrong_contract_version() {
+    let mut deps = mock_dependencies(&[]);
+    cw2::set_contract_version(deps.as_mut().storage, "crates.io:mars-red-bank", "2.3.0").unwrap();
+
+    let err = migrate(deps.as_mut(), mock_env(), MigrateMsg::V2_3_1ToV2_3_2 {}).unwrap_err();
+
+    assert_eq!(
+        err,
+        ContractError::Version(VersionError::WrongVersion {
+            expected: "2.3.1".to_string(),
+            found: "2.3.0".to_string()
+        })
+    );
+}
+
+#[test]
+fn v2_3_1_to_v2_3_2_successful_migration() {
+    let mut deps = mock_dependencies(&[]);
+    cw2::set_contract_version(deps.as_mut().storage, "crates.io:mars-red-bank", "2.3.1").unwrap();
+
+    let res = migrate(deps.as_mut(), mock_env(), MigrateMsg::V2_3_1ToV2_3_2 {}).unwrap();
+
+    assert_eq!(res.messages, vec![]);
+    assert_eq!(res.events, vec![] as Vec<Event>);
+    assert!(res.data.is_none());
+    assert_eq!(
+        res.attributes,
+        vec![attr("action", "migrate"), attr("from_version", "2.3.1"), attr("to_version", "2.3.2")]
+    );
+
+    let new_contract_version = ContractVersion {
+        contract: "crates.io:mars-red-bank".to_string(),
+        version: "2.3.2".to_string(),
+    };
+    assert_eq!(cw2::get_contract_version(deps.as_ref().storage).unwrap(), new_contract_version);
+}
